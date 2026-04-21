627c8c8c7e44b2dd62c9b3f76b4b68758a1637ad
IrfanMuhmd <irfan.m@scalgo.net>
2026-04-21 18:05:11 +0530

    User register and login, model creations

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import UserRegistrationView, CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='login'),
+    
+    # Essential for mobile: allows the app to get a new access token 
+    # without making the user type their password again.
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # This line tells Django to look at acounts app urls
+    path('api/', include('accounts.urls')), 
 ]
