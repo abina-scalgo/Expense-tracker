ebb54b57f563b1045a47fb4c3bc0bf725a0cd855
IrfanMuhmd <irfan.m@scalgo.net>
2026-04-29 10:47:02 +0530

    Fixed gitignore and local settings issue

---
+++ config/sample_local_settings.py
@@ -0,0 +1,15 @@
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
+    }
+}
