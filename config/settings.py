1c5c0cde234e291df4e251c76db194d11c301cae
IrfanMuhmd <irfan.m@scalgo.net>
2026-05-08 12:51:05 +0530

    Fixed conflict issues

--- config/settings.py
+++ config/settings.py
@@ -96,20 +96,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'expense_tracker_db',
-        'USER': 'root',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'expense_tracker_db',
+#         'USER': 'root',
+#         'PASSWORD': '1234',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#             'charset': 'utf8mb4',
+#         },
+#     }
+# }
 
 
 
@@ -156,3 +156,13 @@ import os
 # Where Django stores uploaded files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    raise Exception("A local_settings.py file is required. "
+                    "Copy sample_local_settings.py to local_settings.py to start.")
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
