6eba087c30966cbc7e108aca5ffde960783fadf6
IrfanMuhmd <irfan.m@scalgo.net>
2026-05-19 17:59:05 +0530

    Created SystemSettings API and Force Auto Approval API

--- core/views.py
+++ core/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from .models import SystemSettings
+from .serializers import SystemSettingsSerializer
+from accounts.permissions import IsAdminRoleOrReadOnly
 
-# Create your views here.
+class SystemSettingsViewSet(viewsets.GenericViewSet):
+    queryset = SystemSettings.objects.all()
+    serializer_class = SystemSettingsSerializer
+    permission_classes = [IsAdminRoleOrReadOnly]
+
+    def list(self, request):
+        settings = self.get_queryset()
+
+        serializer = self.get_serializer(settings, many=True)
+        return Response(serializer.data)
+
+
+    def update(self, request, pk=None):
+        try:
+            settings = SystemSettings.objects.get(key=pk)
+        except SystemSettings.DoesNotExist:
+            return Response({"detail": "Settings not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        value = request.data.get("value")
+        if value is None:
+            return Response({"detail": "value is required"}, status=status.HTTP_400_BAD_REQUEST)
+        settings.value = value
+        settings.save()
+        serializer = self.get_serializer(settings)
+        return Response(serializer.data)
